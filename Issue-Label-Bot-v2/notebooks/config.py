4abb6ce2b063214358a2fd00d5b6b7682d0c957d
Chun-Hsiang Wang <chunhsiang@google.com>
2019-07-25 10:39:52 -0700

    Remove comments for config..

--- Issue-Label-Bot-v2/notebooks/config.py
+++ Issue-Label-Bot-v2/notebooks/config.py
@@ -26,26 +26,6 @@ class Config:
         self.labels_local_path = f'{self.repo_name}.labels.dpkl'
         self.emb_local_path = f'{self.repo_name}.emb.dpkl'
 
-        ### repo_owner = config['repository']['owner']
-        ### repo_name = config['repository']['name']
-
-        ### all_config = {}
-        ### all_config['repo_owner'] = config['repository']['owner']
-        ### all_config['repo_name'] = config['repository']['name']
-
-        ### all_config['model_bucket_name'] = 'repo-models'
-        ### all_config['emb_bucket_name'] = 'repo-embeddings'
-
-        ### all_config['model_gcs_path'] = f'{repo_owner}/{repo_name}.model'
-        ### all_config['labels_gcs_path'] = f'{repo_owner}/{repo_name}.labels'
-        ### all_config['emb_gcs_path'] = f'{repo_owner}/{repo_name}'
-
-        ### all_config['model_local_path'] = f'{repo_name}.dpkl'
-        ### all_config['labels_local_path'] = f'{repo_name}.labels.dpkl'
-        ### all_config['emb_local_path'] = f'{repo_name}.emb.dpkl'
-
-        ### return all_config
-
 
 if __name__ == '__main__':
     config = Config(yaml_path='issue_label_bot.yaml')
