008ae634f4d6e9143f9ba11ee58a33de3d1854f0
Chun-Hsiang Wang <chunhsiang@google.com>
2019-07-26 17:16:57 -0700

    Allow loading yaml from gcs.

--- Issue-Label-Bot-v2/notebooks/config.py
+++ Issue-Label-Bot-v2/notebooks/config.py
@@ -1,12 +1,25 @@
 import os
 import yaml
+from google.cloud import storage
 
 class Config:
 
-    def __init__(self, yaml_path=None):
+    def __init__(self, yaml_path=None, owner=None, repo=None):
         assert yaml_path, "No configure yaml path"
         self.yaml_path = yaml_path
+
+        if owner and repo:
+            self.load_yaml_from_gcs(owner, repo)
         self.load()
+
+    def load_yaml_from_gcs(self, owner, repo):
+        self.yaml_bucket_name = 'repo-yaml'
+        self.yaml_dest = f'{owner}/{repo}/issue_label_bot.yaml'
+        storage_client = storage.Client()
+        bucket = storage_client.get_bucket(self.yaml_bucket_name)
+        blob = bucket.get_blob(self.yaml_dest)
+        with open(self.yaml_path, 'wb') as f:
+            blob.download_to_file(f)
 
     def load(self):
         with open(self.yaml_path, 'r') as f:
