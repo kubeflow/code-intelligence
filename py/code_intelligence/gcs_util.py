073d2d2f5b9be5282cef84f6d15db9d1bd1e1a37
Jeremy Lewi <jeremy+github@lewi.us>
2020-04-11 17:31:47 -0700

    Compute embeddings for all kubeflow repositories. (#124)
    * This is the first step in creating an org wide model for all of Kubeflow (#110)
    
    * Modify the Get-GitHub-Issues.ipynb model to reuse code in the embeddings directory.
      * Add some missing packages to requirements.txt.
      * Use the GitHub graphql client to get a list of all repositories.
    
    * Add some missing GCS utilities
    
    * Remove some of the duplication between Get-GitHub-Issues.ipynb and our
      library methods (#122)
    
    * Start fetching the data from bigquery.
    
      * Using BigQuery turned out to be a lot better for bulk pulling all
        of the Kubeflow issues.
    
    * Use hdf5 to save the data.
    
    * Start a doc to keep track of notes for how to train a Kubeflow model. Add logic to save to hDF5 and do a sanity check compared to the inference code
    
    * Add a function to fetch the data using the GitHub API
      * related to https://github.com/kubeflow/code-intelligence/issues/126 swiching the embeddings service to use the GraphQL API rather than html fetching
      * I added this function as a way to sanity check that we get the same data
        using bigquery as at inference time.

--- py/code_intelligence/gcs_util.py
+++ py/code_intelligence/gcs_util.py
@@ -1,5 +1,32 @@
 from google.cloud import storage
 import logging
+import re
+
+GCS_REGEX = re.compile("gs://([^/]*)(/.*)?")
+
+def split_gcs_uri(gcs_uri):
+  """Split a GCS URI into bucket and path."""
+  m = GCS_REGEX.match(gcs_uri)
+  bucket = m.group(1)
+  path = ""
+  if m.group(2):
+    path = m.group(2).lstrip("/")
+  return bucket, path
+
+def check_gcs_object(gcs_path, storage_client=None):
+    """Check if the file exists in the bucket in GCS.
+  
+    Args:      
+      gcs_path: the file name that needs to be checked in GCS, str
+      storage_client: client to bundle configuration needed for API requests
+
+    Return
+    ------
+    bool
+        the file exists in the bucket or not
+    """
+    bucket_name, gcs_file_name = split_gcs_uri(gcs_path)
+    return check_file_in_gcs(bucket_name, gcs_file_name, storage_client=storage_client)
 
 def check_file_in_gcs(bucket_name, gcs_filename, storage_client=None):
     """
@@ -19,6 +46,16 @@ def check_file_in_gcs(bucket_name, gcs_filename, storage_client=None):
     bucket = storage_client.get_bucket(bucket_name)
     return storage.Blob(bucket=bucket, name=gcs_filename).exists(storage_client)
 
+def copy_to_gcs(local_file, gcs_path, storage_client=None):
+    """ Upload a local file to GCS.
+    Args:
+      local_filename: the local file that needs to be uploaded, str      
+      gcs_filename: the file name that is stored in GCS, str      
+      storage_client: client to bundle configuration needed for API requests
+    """
+    bucket_name, gcs_file_name = split_gcs_uri(gcs_path)
+    return upload_file_to_gcs(bucket_name, gcs_file_name, local_file, storage_client=storage_client)
+  
 def upload_file_to_gcs(bucket_name, gcs_filename, local_filename, storage_client=None):
     """
     Upload a local file to GCS.
